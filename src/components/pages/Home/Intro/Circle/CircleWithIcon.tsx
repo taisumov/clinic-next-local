e8ff264461f3f63f8d8636461c677319fe7e7473
taisumov <islam.taisumov10@gmail.com>
2023-02-23 03:28:41 +0300

    commit

--- src/components/pages/Home/Intro/Circle/CircleWithIcon.tsx
+++ src/components/pages/Home/Intro/Circle/CircleWithIcon.tsx
@@ -3,12 +3,18 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 import cx from './index.module.scss';
+import {useDataContext} from "@/context/DataContext";
 
-export const CircleWithIcon = ({ imgSrc, text, rightSide, href }: any) => (
-	<li className={cx('Root')}>
-		<Link href={href}>
-			<Image className={cx('image')} alt="picture" src={imgSrc} fill />
-			<p className={cx('text', rightSide ? 'right' : 'left')}>{text}</p>
-		</Link>
-	</li>
-);
+export const CircleWithIcon = ({ imgSrc, text, rightSide, href }: any) => {
+
+	const { about } = useDataContext();
+
+	return (
+		<li className={cx('Root')}>
+			<Link href={{pathname: href, query: JSON.stringify(about)}}>
+				<Image className={cx('image')} alt="picture" src={imgSrc} fill/>
+				<p className={cx('text', rightSide ? 'right' : 'left')}>{text}</p>
+			</Link>
+		</li>
+	);
+}
--- src/components/pages/Services/Services.tsx
+++ src/components/pages/Services/Services.tsx
@@ -5,15 +5,24 @@ import { ServicesList } from '@/components/base/ServicesList';
 import { SERVICES_LIST, TITLE } from './constants';
 
 import cx from './index.module.scss';
+import {useRouter} from "next/router";
 
-export const Services = () => (
-	<main className={cx('Root')}>
-		<Region className={cx('title')}>
-			<Heading className={cx('title__head')}> {TITLE.first}</Heading>
-			<Heading className={cx('title__head')}> {TITLE.second}</Heading>
-			<hr className={cx('hr')} />
-			<hr className={cx('hr')} />
-		</Region>
-		<ServicesList arr={SERVICES_LIST} />
-	</main>
-);
+export const Services = () => {
+
+	const router = useRouter();
+	const about = router.query;
+
+	console.log(JSON.stringify(about))
+
+	return (
+		<main className={cx('Root')}>
+			<Region className={cx('title')}>
+				<Heading className={cx('title__head')}> {TITLE.first}</Heading>
+				<Heading className={cx('title__head')}> {TITLE.second}</Heading>
+				<hr className={cx('hr')}/>
+				<hr className={cx('hr')}/>
+			</Region>
+			<ServicesList arr={SERVICES_LIST}/>
+		</main>
+	)
+}
