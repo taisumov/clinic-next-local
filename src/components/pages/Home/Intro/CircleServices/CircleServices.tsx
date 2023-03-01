a0eab8060aaf16d8a519f76567ef01343aecbe4e
taisumov <islam.taisumov10@gmail.com>
2023-03-02 00:13:24 +0300

    Changes in some styles.

--- src/components/pages/Home/Intro/CircleServices/CircleServices.tsx
+++ src/components/pages/Home/Intro/CircleServices/CircleServices.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { useState } from 'react';
 
 import { useDataContext } from '@/context/DataContext';
@@ -10,73 +11,91 @@ import cx from './index.module.scss';
 
 export const CircleServices = () => {
 	const { categories } = useDataContext();
-	const [uslugi, setUslugi] = useState(categories.data);
+	const [currentLevel, setCurrentLevel] = useState(categories.data);
 	const [selectedCategory, setSelectedCategory] = useState('');
 
-	const deeep = (uuu: any) => {
-		setUslugi(uuu);
-		// setSelectedCategory(item.attributes.text);
+	const handleChangeLevel = (circles: any) => {
+		setCurrentLevel(circles);
 	};
 
 	return (
-		<div className={cx('Root')}>
-			<ul
-				className={cx('circle')}
-				style={
-					selectedCategory.length
-						? {
+		<div className={cx('RootRoot')}>
+			<div className={cx('Root')}>
+				<ul
+					className={cx('circle')}
+					style={
+						selectedCategory.length
+							? {
 								backgroundImage: `url(${circleImage.src})`,
 								backgroundPosition: 'bottom center',
 								backgroundSize: 'contain',
-						  }
-						: {}
-				}
-			>
-				{uslugi.map((item: any, index: number) => {
-					if (item.attributes?.subcategories) {
+							}
+							: {}
+					}
+				>
+					{currentLevel.slice(0, 5).map((item: any, index: number) => {
+						if (item.attributes?.subcategories) {
+							return (
+								<Circle
+									onClick={handleChangeLevel}
+									subItems={item.attributes.subcategories.data}
+									key={item.id}
+									imgSrc={item.attributes.bg.data.attributes.url}
+									text={item.attributes.text}
+									rightSide={index < 3}
+									setSelectedCategory={setSelectedCategory}
+								/>
+							);
+						}
+
 						return (
-							<Circle
-								onClick={deeep}
-								subItems={item.attributes.subcategories.data}
+							<CircleWithIcon
+								href={'/services'}
 								key={item.id}
-								imgSrc={item.attributes.bg.data.attributes.url}
+								imgSrc={
+									'https://res.cloudinary.com/dsrqil9aq/image/upload/v1677525509/Copy_Copy_Ellipse_49_587c156bd1.png?updated_at=2023-02-27T19:18:29.903Z'
+								}
 								text={item.attributes.text}
 								rightSide={index < 3}
-								setSelectedCategory={setSelectedCategory}
 							/>
 						);
-					}
-
-					return (
-						<CircleWithIcon
-							href={'/services'}
-							key={item.id}
-							imgSrc={
-								'https://res.cloudinary.com/dsrqil9aq/image/upload/v1677525509/Copy_Copy_Ellipse_49_587c156bd1.png?updated_at=2023-02-27T19:18:29.903Z'
-							} // item.attributes.bg.data[0].attributes.url}
-							text={item.attributes.text}
-							rightSide={index < uslugi.length / 2}
-						/>
-					);
-				})}
-			</ul>
-			<div
-				className={cx('textContainer')}
-				style={selectedCategory === '' ? { top: '25%', left: '38%' } : {}}
-			>
-				{selectedCategory.length ? (
-					selectedCategory
-				) : (
-					<></>
-					// <Image
-					// 	layout={'intrinsic'}
-					// 	src={logo}
-					// 	alt="logo"
-					// 	width={400}
-					// 	height={400}
-					// />
-				)}
+					})}
+				</ul>
+				<div
+					onClick={() => {
+						setCurrentLevel(categories.data)
+						setSelectedCategory('')
+					}}
+					className={cx('textContainer')}
+					style={selectedCategory === '' ? { top: '25%', left: '38%' } : {}}
+				>
+					{selectedCategory.length ? (
+						selectedCategory
+					) : (
+						<></>
+					)}
+				</div>
 			</div>
+			{currentLevel.length > 6 &&
+				<div className={cx('extraItemsContainer')}>
+					<div className={cx('extraItems')}>
+						{currentLevel.slice(4).map((item: any) =>
+							<div key={item.attributes.text} className={cx('extraItem')}>
+								<div className={cx('extraImageContainer')}>
+									<Image
+										className={cx('extraImage')}
+										alt="picture"
+										src={'https://res.cloudinary.com/dsrqil9aq/image/upload/v1677525509/' +
+											'Copy_Copy_Ellipse_49_587c156bd1.png?updated_at=2023-02-27T19:18:29.903Z'}
+										fill
+									/>
+								</div>
+								<div>{item.attributes.text}</div>
+							</div>
+						)}
+					</div>
+				</div>
+			}
 		</div>
 	);
 };
